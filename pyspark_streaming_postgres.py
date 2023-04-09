f164dc773d1530bc7426310281aba5d4de64ff8b
Higor Silva <higor.silva@ammovarejo.com.br>
2023-04-09 04:09:33 -0300

    feat(pyspark_streaming_postgres): Adicionando stream ao banco postgres

---
+++ pyspark_streaming_postgres.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName('app_name') \
+    .config("spark.driver.extraClassPath", "jdbc_drivers/postgresql-42.2.24.jar") \
+    .getOrCreate()
+
+schema = 'nome STRING, postagem STRING, data INT'
+
+df = spark.readStream.json(path='dados_de_exemplo/streaming_teste/',
+                           schema=schema)
+
+diretorio = 'temp'
+
+
+def update_postgres(df):
+    df.write.format('jdbc') \
+        .option('url', 'jdbc:postgresql://postgres-database.chcgqj6jrjf8.us-west-2.rds.amazonaws.com/posts') \
+        .option('dbtable', 'posts') \
+        .option('user', 'postgres') \
+        .option('password', '123456789') \
+        .option('driver', 'org.postgresql.Driver') \
+        .option('mode', 'append') \
+        .save()
+   
+    stcal = df.writeStream.foreachBath(update_postgres) \
+        .outputMode('append') \
+        .trigger(processingTime='5 second') \
+        .option('checkopointlocation', diretorio) \
+        .start()
+    
+    stcal.awaitTermination()
